979b056b4d818f16964ce5061d2bccf019384393
zainalsalamun <zainalrtf@gmail.com>
2025-12-08 15:29:04 +0700

    update : main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shoppe_clone/page/livin_home_page.dart';
+import 'package:shoppe_clone/page/gojek_home_page.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LivinHomePage()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: GojekHomePage()));
 }
