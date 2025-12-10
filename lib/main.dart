9e31c07453b85f2e9d6a74112db61925c2d95b9c
zainalsalamun <zainalrtf@gmail.com>
2025-12-10 14:48:30 +0700

    update : main all demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:shoppe_clone/page/gojek_home_page.dart';
+import 'package:shoppe_clone/page/all_pages_demo.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: GojekHomePage()));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const AllPagesDemo(),
+    );
+  }
 }
