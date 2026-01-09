9907f1d701304511628b2cd6a8e9379c9231b13d
zainalsalamun <zainalrtf@gmail.com>
2026-01-09 14:10:03 +0700

    add : form page

---
+++ lib/page_menu/talenta/menu/forms_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FormsPage extends StatelessWidget {
+  const FormsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Forms'),
+        backgroundColor: const Color(0xFFF6F6F6),
+        foregroundColor: const Color(0xFF1F1F1F),
+        elevation: 0,
+      ),
+      body: const Center(child: Text('Forms Content')),
+    );
+  }
+}
