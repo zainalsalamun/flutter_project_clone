f671e37878d0115e16595cd20c8f2ce226dc11d7
zainalsalamun <zainalrtf@gmail.com>
2026-02-09 10:26:47 +0700

    feat: Implement navigation to TixIdCinemaDetailPage and adjust import paths for detail pages.

--- lib/page_menu/tix_id/menu/tix_id_rent_list_page.dart
+++ lib/page_menu/tix_id/menu/tix_id_rent_list_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'tix_id_movie_detail_page.dart';
+import '../details/tix_id_movie_detail_page.dart';
 
 class TixIdRentListPage extends StatelessWidget {
   const TixIdRentListPage({super.key});
