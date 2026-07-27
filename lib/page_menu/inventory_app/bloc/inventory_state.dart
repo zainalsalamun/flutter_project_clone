a5e7d02c7c1ee3b97acede1edf26c435310f14bc
zainalsalamun <zainalrtf@gmail.com>
2026-07-27 09:45:22 +0700

    feat: implement TaskManager BLoC architecture, add profile/settings pages, and initialize Nova AI and Inventory state structures.

---
+++ lib/page_menu/inventory_app/bloc/inventory_state.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import '../inventory_models.dart';
+
+abstract class InventoryState extends Equatable {
+  const InventoryState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class InventoryInitial extends InventoryState {}
+
+class InventoryLoading extends InventoryState {}
+
+class InventoryLoaded extends InventoryState {
+  final List<InventoryItem> items;
+
+  const InventoryLoaded(this.items);
+
+  @override
+  List<Object> get props => [items];
+}
+
+class InventoryError extends InventoryState {
+  final String message;
+
+  const InventoryError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
