ab0fc9debd600252426a04fef022343c35b111b3
zainalsalamun <zainalrtf@gmail.com>
2026-08-02 16:03:42 +0700

    feat: scaffold modular architecture for multiple app demonstrations and domain-specific feature sets

--- lib/page/all_pages_demo.dart
+++ lib/page/all_pages_demo.dart
@@ -28,6 +28,7 @@ import 'package:project_clone/page_menu/food_delivery_app/food_delivery_app.dart
 import 'package:project_clone/page_menu/pos_app/pos_app.dart';
 import 'package:project_clone/page_menu/hospital_reservation_app/hospital_app.dart';
 import 'package:project_clone/page_menu/rs_medika_app/rs_medika_app.dart';
+import 'package:project_clone/page_menu/bca_mobile_app/pages/m_bca_page.dart';
 
 class AllPagesDemo extends StatelessWidget {
   const AllPagesDemo({super.key});
@@ -255,7 +256,13 @@ class AllPagesDemo extends StatelessWidget {
             iconColor: Colors.green,
             page: const RsMedikaApp(),
           ),
-
+          _menuCard(
+            context,
+            title: "myABC Clone",
+            icon: Icons.account_balance_wallet,
+            iconColor: const Color(0xFF005AA9),
+            page: const MBcaPageWrapper(),
+          ),
         ],
       ),
     );
