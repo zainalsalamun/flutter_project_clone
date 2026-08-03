1eda256fa4d24fcbb0313ad477f2a0db5c52e350
zainalsalamun <zainalrtf@gmail.com>
2026-08-03 09:47:36 +0700

    feat: implement multi-app architecture modules with comprehensive BLoC patterns, UI pages, and domain models.

---
+++ lib/page_menu/weather_app/bloc/weather_event.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+abstract class WeatherEvent extends Equatable {
+  const WeatherEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FetchWeather extends WeatherEvent {}
