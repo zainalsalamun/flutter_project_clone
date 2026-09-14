8f2bd3240798a099a52bcaad8a5dc1a7bac4ae94
zainalsalamun <zainalrtf@gmail.com>
2026-09-14 10:09:45 +0700

    feat: add new pages, blocs, and components across multiple feature modules

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Project Clone',
       debugShowCheckedModeBanner: false,
       home: const AllPagesDemo(),
     );
